88fd342936252fcfbd0819e127692f5ce7c06a1a
cserrano-301 <54609939+cserrano-301@users.noreply.github.com>
2019-09-20 13:47:32 -0400

    Update Meditation Poetry.py

--- poem/Meditation Poetry.py
+++ poem/Meditation Poetry.py
@@ -5,9 +5,37 @@ import random
 
 wordlist = text.replace("\n"," ").split(" ")
 
+print( "\n")
 seed = random.randint(0, len(wordlist)-1)
 wordlist[seed:seed+80]
-line1=wordlist[seed:seed+1]
+line1=wordlist[seed:seed+3]
+print(" ".join(line1) )
+
+seed2 = random.randint(0, len(wordlist)-1)
+wordlist[seed2:seed2+66]
+line2=wordlist[seed2:seed2+6]
+print(" ".join(line2) )
+
+seed3 = random.randint(0, len(wordlist)-1)
+wordlist[seed3:seed3+99]
+line3=wordlist[seed3:seed3+5]
+print(" ".join(line3) )
+
+seed4 = random.randint(0, len(wordlist)-1)
+wordlist[seed4:seed4+102]
+line4=wordlist[seed4:seed4+6]
+print(" ".join(line4) )
+
+seed5 = random.randint(0, len(wordlist)-1)
+wordlist[seed5:seed5+77]
+line5=wordlist[seed5:seed5+5]
+print(" ".join(line5) )
+
+print( "\n")
+
+seed = random.randint(0, len(wordlist)-1)
+wordlist[seed:seed+80]
+line1=wordlist[seed:seed+5]
 print(" ".join(line1) )
 
 seed2 = random.randint(0, len(wordlist)-1)
@@ -17,47 +45,19 @@ print(" ".join(line2) )
 
 seed3 = random.randint(0, len(wordlist)-1)
 wordlist[seed3:seed3+99]
-line3=wordlist[seed3:seed3+3]
+line3=wordlist[seed3:seed3+5]
 print(" ".join(line3) )
 
 seed4 = random.randint(0, len(wordlist)-1)
 wordlist[seed4:seed4+102]
-line2=wordlist[seed4:seed4+2]
-print(" ".join(line2) )
-
-seed5 = random.randint(0, len(wordlist)-1)
-wordlist[seed5:seed5+77]
-line4=wordlist[seed5:seed5+4]
+line4=wordlist[seed4:seed4+5]
 print(" ".join(line4) )
 
 print( "\n")
 
 seed = random.randint(0, len(wordlist)-1)
 wordlist[seed:seed+80]
-line1=wordlist[seed:seed+3]
-print(" ".join(line1) )
-
-seed2 = random.randint(0, len(wordlist)-1)
-wordlist[seed2:seed2+66]
-line2=wordlist[seed2:seed2+4]
-print(" ".join(line2) )
-
-seed3 = random.randint(0, len(wordlist)-1)
-wordlist[seed3:seed3+99]
-line3=wordlist[seed3:seed3+3]
-print(" ".join(line3) )
-
-seed4 = random.randint(0, len(wordlist)-1)
-wordlist[seed4:seed4+102]
-line2=wordlist[seed4:seed4+2]
-print(" ".join(line2) )
-
-
-print( "\n")
-
-seed = random.randint(0, len(wordlist)-1)
-wordlist[seed:seed+80]
-line1=wordlist[seed:seed+3]
+line1=wordlist[seed:seed+6]
 print(" ".join(line1) )
 
 seed2 = random.randint(0, len(wordlist)-1)
@@ -67,17 +67,17 @@ print(" ".join(line2) )
 
 seed3 = random.randint(0, len(wordlist)-1)
 wordlist[seed3:seed3+99]
-line3=wordlist[seed3:seed3+3]
+line3=wordlist[seed3:seed3+6]
 print(" ".join(line3) )
 
 seed4 = random.randint(0, len(wordlist)-1)
 wordlist[seed4:seed4+102]
-line2=wordlist[seed4:seed4+2]
-print(" ".join(line2) )
+line4=wordlist[seed4:seed4+5]
+print(" ".join(line4) )
 
 print( "\n")
 
 seed5 = random.randint(0, len(wordlist)-1)
 wordlist[seed5:seed5+77]
-line4=wordlist[seed5:seed5+3]
-print(" ".join(line4) )
+line5=wordlist[seed5:seed5+3]
+print(" ".join(line5) )
