908821126fbd81dad846d630d9aa09de93c7d70a
cserrano-301 <54609939+cserrano-301@users.noreply.github.com>
2019-12-08 19:10:34 -0500

    Create HorsingAround.js

---
+++ screensaver/HorsingAround.js
@@ -0,0 +1,88 @@
+//Horse Photo by Mikael Kristenson on Unsplash
+let horse;
+let a = -0.07;
+let s = 0.02;
+
+function preload() {
+  horse = loadImage('horse.jpg');
+}
+  var col = {
+  r: 219,
+  g: 210,
+  b: 25
+}
+
+
+function setup() {
+  createCanvas(800, 800, WEBGL);
+  //https://p5js.org/reference/#/p5/rectMode - adjusted perameters
+  rectMode(CENTER);
+  col.r = random (150,218);
+  col.g = random (60,130);
+  col.b = random (10,22,0);
+}
+
+function draw() {
+  background(0);
+  //rectMode(CENTER);
+  //https://p5js.org/examples/transform-scale.html
+  a = a + 0.01;
+  s = cos(a) * 1.5;
+  translate(width / -50, height / -50);
+  scale(s);
+  frameRate(75);
+  
+  
+  //p5js reference - 3D
+  rotateX(millis() / 1000);
+  rotateX(PI / -4);
+  //blue boxes
+  stroke(77,102,220);
+  box(355);
+  let rad = millis() / 1200;
+  // Set rotation angles
+  let ct = cos(rad);
+  let st = sin(rad);
+  // Matrix for rotation around the Y axis
+  applyMatrix(  ct, 0.0,  st,  0.0,
+               0.0, 1.0, 0.0,  0.0,
+               -st, 0.0,  ct,  0.0,
+               0.0, 0.0, 0.0,  1.0);
+  rotateX(millis() / 1000);
+  rotateY(PI / 4);
+  box(250);
+  rotateX(PI / 8);
+  stroke(255);
+  box(325);
+  fill(col.r,col.g,col.b,25);
+  stroke(195,120,163);
+  rotateX(PI / 4);
+  
+  //p5js reference
+  //specularMaterial(250,150);
+  texture(horse);
+  box(125);
+  stroke(255);
+  rotateX(millis() / 1000);
+  rotateY(PI / 6);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.06);
+  
+  
+  col.r = random (250,218);
+  col.g = random (30,10);
+  col.b = random (150,225,200);
+  //shape of inside sphere
+  noFill();
+  
+  sphere(50,5,4);
+    
+  //sphere color blue
+  stroke(85,175,255);
+  fill(col.r,col.g,col.b,175);
+  sphere(24,24,15);
+  noStroke();
+  fill(col.r,col.g,col.b,10);
+  
+}
+
